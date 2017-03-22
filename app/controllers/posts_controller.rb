3c6f65eaccfd8bc0047314e71e3688dd34c46177
haoran <eri.c@163.com>
2017-03-22 14:23:39 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,34 @@
 class PostsController < ApplicationController
 before_action :authenticate_user!, :only => [:new, :create]
 
+def edit
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.group = @group
+end
+
+def update
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.group = @group
+  @post.user = current_user
+
+  if @post.update(post_params)
+    redirect_to account_posts_path, notice:"编辑成功"
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.group = @group
+  @post.destroy
+  flash[:alert] = "Post deleted"
+  redirect_to account_posts_path
+end
+
  def new
    @group = Group.find(params[:group_id])
    @post = Post.new
@@ -19,10 +47,11 @@ before_action :authenticate_user!, :only => [:new, :create]
    end
  end
 
+
  private
 
  def  post_params
    params.require(:post).permit(:content)
  end
- 
+
  end
